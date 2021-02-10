aaba23cd209f03b7c9278bfc1089ebc1e69d79fb
rohit kumar <rohit.kumar@innoplexus.com>
2021-02-10 10:40:53 +0530

    fix: test1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Release please testing 1: fix
         </p>
         <a
           className="App-link"
